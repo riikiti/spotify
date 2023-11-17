e635f0d1dd760e0546f973c832b851a5b5e5ddda
r.kurskiy <r.kurskiy@msocialproduction.com>
2023-11-17 13:32:19 +0300

    #2 #init #add - fix migration rollback

--- database/factories/MusicsFactory.php
+++ database/factories/MusicsFactory.php
@@ -5,9 +5,9 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Music>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Musics>
  */
-class MusicFactory extends Factory
+class MusicsFactory extends Factory
 {
     /**
      * Define the model's default state.
--- database/migrations/2023_11_17_072905_create_musics_table.php
+++ database/migrations/2023_11_17_072905_create_musics_table.php
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('music');
+        Schema::dropIfExists('musics');
     }
 };
